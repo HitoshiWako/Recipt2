6ef365c3aa052d74d3dea63e4422063e256189f5
Hitoshi Wako <Hitoshi.Wako@gmail.com>
2023-08-14 22:19:23 +0900

    use bootstrap

--- recipt/recipt.py
+++ recipt/recipt.py
@@ -1,4 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from .model import Store, Recipt, Item
 
 bp = Blueprint('recipt',__name__)
+
+@bp.route('/upload',methods=('GET','POST'))
+def upload():
+    return render_template('recipt/upload.html')
